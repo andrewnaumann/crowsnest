6c60579ca3b0fba72d0158938392472c88d976fd
Andrew Naumann <apnaumann@gmail.com>
2016-12-13 19:34:43 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+var bs = require('browser-sync').create();
+
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed' // if css compressed **file size**
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch("*.html").on('change', bs.reload);
+});
---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation();
+
+$('#slick-bacon').slick({
+  autoplay:true,
+  autoplaySpeed: 4000,
+  arrows: false,
+  dots: true,
+  speed: 500
+});
